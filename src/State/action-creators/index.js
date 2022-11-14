eea2552d752c8e7087179bd8103c2a21bb87167b
pandit <dhamdherepandit@gmail.com>
2022-11-14 20:59:51 +0530

    action creator and reducer added

---
+++ src/State/action-creators/index.js
@@ -0,0 +1,17 @@
+export const depositMoney = (ammount) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'deposit',
+            payload: ammount
+        })
+    }
+}
+
+export const withdrawMoney = (ammount) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'withdraw',
+            payload: ammount
+        })
+    }
+}
