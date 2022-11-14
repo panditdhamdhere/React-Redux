8ed4410266748f76c8fe6bc99ba10bbaa176ffbd
pandit <dhamdherepandit@gmail.com>
2022-11-14 20:33:11 +0530

    video 74 react redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./Components/Navbar";
+import Shop from "./Components/Shop";
 
 function App() {
   return (
-    <div className="App">
+    <>
+   
       <Navbar />
+      <div className="container">
+      <Shop />
     </div>
+    </>
   )
 }
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-blue">
+            <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">State Bank Of Pandit</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +19,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                         <div>
-                            <button className="btn-primary">Your Balance: 0</button>
+                            <button disabled={true} className="btn-primary">Your Balance: 10000</button>
                         </div>
                     </div>
                 </div>
